b8985de70f4bf74bdcbf29e0f46826f4198d99a1
iskic650 <i.alimbajew@gmail.com>
2023-07-15 14:14:10 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.addition(10,7));
+        System.out.println(calculator.subtraction(10,7));
+    }
+}
